bcf8287ccef37880f6f7d2b86bc3e06f01c2c2f0
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-30 10:05:23 +0100

    basicAlert added

--- src/app/@shared/modal-upload/modal-upload.service.ts
+++ src/app/@shared/modal-upload/modal-upload.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
+import { AuthService } from 'src/app/services/auth.service';
 import { ApiService } from '../../@graphql/services/api.service';
+import { ShopGuard } from '../../guards/shop.guard';
+import { basicAlert } from '../alerts/toasts';
+import { TYPE_ALERT } from '../alerts/values.config';
+const jwtDecode = require('jwt-decode');
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalUploadService extends ApiService {
 
-  constructor(apollo: Apollo) {
+  constructor(apollo: Apollo, private auth: AuthService) {
     super(apollo)
 }
 
@@ -15,19 +20,21 @@ uploadFile(file: File, type: string, id: string) {
 
     return new Promise((resolve,reject) => {
 
-    
+      this.auth.getSession().token
+
+      let token = this.auth.getSession().token
       let formData = new FormData(); // es el payload que yo quiero mandar subir // Para crear la data que quiero enviar al back
       let xhr = new XMLHttpRequest(); //inicializamos la petición ajax
   
       formData.append('imagen', file, file.name) // 'imagen' es el nombre que le doy en el backend
-  
+
       xhr.onreadystatechange = function() { // esto es la configuracion de la petición ajax, de como va a funcionar
-        if ( xhr.readyState ===4 ) { // el 4 es un estado de la subida, podría jugar con ellos para hacer un loading
+        if ( xhr.readyState === 4 ) { // el 4 es un estado de la subida, podría jugar con ellos para hacer un loading
           if (xhr.status === 200 ) {
-            alert('File Subido');
+            basicAlert(TYPE_ALERT.SUCCESS,'File Subido')
             resolve(JSON.parse(xhr.response));
           } else {
-            alert('Subida Fallida');
+            basicAlert(TYPE_ALERT.ERROR, 'Subida fallida')
             reject(xhr.response)
           }
         }
@@ -37,6 +44,8 @@ uploadFile(file: File, type: string, id: string) {
       let url = `http://localhost:2002/upload/${type}/${id}`
 
       xhr.open('PUT',url, true ); // el true dice si quiero que sea asincrono
+
+      xhr.setRequestHeader('Authorization', token)
 
       xhr.send(formData);
 })
