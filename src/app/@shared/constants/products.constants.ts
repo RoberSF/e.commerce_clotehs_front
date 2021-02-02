c7867952027d46f216d4793cab41cf21eaf391a7
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-02-02 11:59:33 +0100

    more items with categoria only

--- src/app/@shared/constants/products.constants.ts
+++ src/app/@shared/constants/products.constants.ts
@@ -33,20 +33,159 @@ export const PRODUCTS_PAGES_INFO = {
         title: 'Sport',
         description: `En esta página encontrarás ropa para uniformarte para hacer deporte`,
         categoriasId: ['18','19'],
-        topPrice: 45,
+        topPrice: -1,
         },
     'products/headwear': {
         title: 'HeadWear',
         description: `En esta página encontrarás complementos tanto para cuello como para cabeza`,
         categoriasId: ['20','21'],
-        topPrice: 45,
+        topPrice: -1,
         },
     'products/accesorios': {
         title: 'Accesorios',
         description: `En esta página encontrarás complementos de todo tipo`,
         categoriasId: ['22','23','24'],
-        topPrice: 45,
+        topPrice: -1,
         },
+      'products/camisetasMangaCorta': {
+         title: 'Cmisetas de manga corta',
+         description: 'En eta página encontrarás camisetas de manga corta',
+         categoriasId: ['2'],
+         topPrice: -1
+      },
+      'products/camisetasDeTirantes': {
+         title: 'Cmisetas de tirantes',
+         description: 'En eta página encontrarás camisetas de tirantes',
+         categoriasId: ['3'],
+         topPrice: -1
+      },
+      'products/polos': {
+         title: 'Polos',
+         description: 'En eta página encontrarás prendas del tipo Polo',
+         categoriasId: ['4'],
+         topPrice: -1
+      },
+      'products/camisetasYPolos': {
+         title: 'Camisetas y Polos',
+         description: 'En eta página encontrarás camisetas y polos',
+         categoriasId: ['5'],
+         topPrice: -1
+      },
+      'products/chaquetasYsudaderas': {
+         title: 'Chaquetas y Sudaderas',
+         description: 'En eta página encontrarás camisetas y sudaderas',
+         categoriasId: ['6'],
+         topPrice: -1
+      },
+      'products/polares': {
+         title: 'Polares',
+         description: 'En eta página encontrarás polares para abrigarte',
+         categoriasId: ['7'],
+         topPrice: -1
+      },
+      'products/ropaAbrigo': {
+         title: 'Ropa de abrigo',
+         description: 'En eta página encontrarás prendas para abrigarte',
+         categoriasId: ['8'],
+         topPrice: -1
+      },
+      'products/chubasqueros': {
+         title: 'Chubasqueros',
+         description: 'En eta página encontrarás chubasqueros para no mojarte',
+         categoriasId: ['9'],
+         topPrice: -1
+      },
+      'products/camisetasInteriores': {
+         title: 'Camisetas Interiores',
+         description: 'En eta página encontrarás camisetas interiores',
+         categoriasId: ['10'],
+         topPrice: -1
+      },
+      'products/calcetines': {
+         title: 'Calcetines',
+         description: 'En eta página encontrarás calcetines',
+         categoriasId: ['11'],
+         topPrice: -1
+      },
+      'products/tecnica': {
+         title: 'Ropa Técnica',
+         description: 'En eta página encontrarás ropa técnica',
+         categoriasId: ['12'],
+         topPrice: -1
+      },
+      'products/futbol': {
+         title: 'Fútbol',
+         description: 'En eta página encontrarás ropa relacionado con el fútbol',
+         categoriasId: ['13'],
+         topPrice: -1
+      },
+      'products/pantalonesDeportivos': {
+         title: 'Pantalones Deportivos',
+         description: 'En eta página encontrarás pantalones deportivos para sentirte cómodo',
+         categoriasId: ['14'],
+         topPrice: -1
+      },
+      'products/hightTech': {
+         title: 'Hight Tech',
+         description: 'En eta página encontrarás ropa de alta tecnología',
+         categoriasId: ['15'],
+         topPrice: -1
+      },
+      'products/chandals': {
+         title: 'Chandals',
+         description: 'En eta página encontrarás chandals',
+         categoriasId: ['16'],
+         topPrice: -1
+      },
+      'products/bañadores': {
+         title: 'Bañadores',
+         description: 'En eta página encontrarás bañadores',
+         categoriasId: ['17'],
+         topPrice: -1
+      },
+      'products/visibilidad': {
+         title: 'Alta visibilidad',
+         description: 'En eta página encontrarás prendas de alta visibilidad',
+         categoriasId: ['18'],
+         topPrice: -1
+      },
+      'products/uniformidad': {
+         title: 'Uniformidad',
+         description: 'En eta página encontrarás prendas para uniformarte',
+         categoriasId: ['19'],
+         topPrice: -1
+      },
+      'products/accesoriosCuello': {
+         title: 'Accesorios para el cuello',
+         description: 'En eta página encontrarás accesorios para proteger y adornar tu cuello',
+         categoriasId: ['20'],
+         topPrice: -1
+      },
+      'products/gorras': {
+         title: 'Gorras',
+         description: 'En eta página encontrarás gorras de varios estilos',
+         categoriasId: ['21'],
+         topPrice: -1
+      },
+      'products/accesoriosYcomplementos': {
+         title: 'Accesorios y complementos',
+         description: 'En eta página encontrarás accesorios y complementos de varios estilos',
+         categoriasId: ['22'],
+         topPrice: -1
+      },
+      'products/bolsas': {
+         title: 'Bolsas',
+         description: 'En eta página encontrarás bolsas para guardar cualquier cosa que necesites',
+         categoriasId: ['23'],
+         topPrice: -1
+      },
+      'products/bolsasNoWomen': {
+         title: 'Bolsas Non Women',
+         description: 'En eta página encontrarás mas estilos de bolsas',
+         categoriasId: ['24'],
+         topPrice: -1
+      },
+      
 };
 
 export enum TYPE_OPERATION {
