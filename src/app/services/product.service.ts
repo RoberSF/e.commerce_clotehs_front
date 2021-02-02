cfed835cbedffb4f1b9ecb875a6810743382b58c
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-02-02 11:58:51 +0100

    deleted things

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -44,30 +44,8 @@ export class ProductService extends ApiService {
 //       }));
 // }
 
-getHomePage() {
-  return this.get(HOME_PAGE, {showPlatform: true}).pipe(map( (result:any) => {
-      return {
-        carousel: result.carousel,
-        ps4: this.manageInfo(result.ps4.products, true),
-        pc: this.manageInfo(result.pc.products, true),
-        topPrice: this.manageInfo(result.topPrice35.products, true)
-      };
-  }))
-}
 
 
-// getItem(id: number) {
-
-//   return this.get( PRODUCT_DETAILS, { id: id}, {}, false).pipe(map( (result: any) => {
-//     const data =  result.productDetails;
-//     return {
-//       product: this.setInObject(data.product, true),
-//       screenshoots: data.product.product.screenshoot, //ojo al doble product que es a causa del fragment
-//       similarProducts: data.product.similarProducts
-//     } 
-//   }))
-
-// }
 
 // itemsRandom() {
 //   return this.get(PRODUCT_RANDOM).pipe(map((result:any) => {
@@ -76,47 +54,11 @@ getHomePage() {
 //   }))
 // }
 
-stockUpdateListener(id: number) {
-  return this.subscription(SUBSCRIPTIONS_PRODUCT_SELECT_STOCK, { id }).pipe(map( (result: any) => {
-    return result.selectProductStockUpdate
-  }));
-}
-
-  getByPlatformSearch(page: number = 1, itemsPage: number = 10, active: ACTIVE_FILTERS = ACTIVE_FILTERS.ACTIVE, platform: Array<string>, searchValue: string) {
-    return this.get(SEARCH_PRODUCTO_PLATFORM, { page, itemsPage, active, platform, searchValue }).pipe(map((result: any) => {
-      const data = result.productsPlatformsSearch
-      return {
-        info: data.info,
-        result: this.manageInfo(data.products)
-      }
-
-    }));
-  }
-
-
-private setInObject(productObject, showDescription) {
-  return {
-    // Tener en cuenta la interfaceIProduct
-    id: productObject.id,
-    img: productObject.product.img,
-    name: productObject.product.name,
-    rating: productObject.product.rating,
-    description: (productObject.platform && showDescription) ? productObject.platform.name : '',
-    qty: 1,
-    price: productObject.price,
-    stock: productObject.stock
-  };
-}
 
 
 
-private manageInfo(listProducts, showDescription = true) {
-        const resultList: Array<IProduct> = [];
-        listProducts.map( (productObject) => {
-          // resultList.push( this.setInObject(productObject, showDescription)) // Daría error por la nueva IProduct que he definido
-        })
-        return resultList;
-}
+
+
 
 
 }
